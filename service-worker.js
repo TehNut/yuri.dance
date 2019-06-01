c48149989db2eeb814d3eeaefafcc9092ea49060
huequica <harumaki1225@gmail.com>
2019-06-01 14:44:22 +0900

    add manifest and  service-worker files

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+});
